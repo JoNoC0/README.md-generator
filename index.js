2454343b5c5f9d9e9c848a238b1ed1b9df386a35
JoNoC0 <jnguyeningco@gmail.com>
2021-11-11 17:47:39 -0600

    added name, type and message values

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// WHEN I enter a description, 
+// installation instructions
+// usage information
+// contribution guidelines
+// test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
@@ -14,6 +29,26 @@ const userQuestions = [{
     name: 'description',
     type: 'input',
     message: 'What is the description of the project?'
+},
+{
+    name: 'instructions',
+    type: 'input',
+    message: 'What is the installation instructions?'
+},
+{
+    name: 'usage',
+    type: 'input',
+    message: 'How to use the project?'
+},
+{
+    name: 'contribution',
+    type: 'input',
+    message: 'Were there contributors to the project? If so, describe contribution(s) made.'
+},
+{
+    name: 'testInstructions',
+    type: 'input',
+    message: 'What is the instructions for testing?'
 }];
 
 // TODO: Create a function to write README file
